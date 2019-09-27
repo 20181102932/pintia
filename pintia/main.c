3d5b9b70e4765c444e4ec6cd896e42511ddc1c34
20181102932 <908907990@qq.com>
2019-09-27 17:21:51 +0800

    Update main.c

--- pintia/main.c
+++ pintia/main.c
@@ -1,15 +1,56 @@
-//
-//  main.c
-//  pintia
-//
-//  Created by s20181102932 on 2019/9/27.
-//  Copyright © 2019 wjx. All rights reserved.
-//
-
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+bool Push( ElementType X, Deque D )
+{
+    if ((D->Rear + 1) % D->MaxSize == D->Front)
+    {
+        return -1;
+    }
+    else {
+        Deque E = (Deque)malloc(sizeof(struct QNode));
+        E->Rear=D;
+        D=E;
+        E->Data[D-1]=X;
+        return 1;
+    }
+}
+ElementType Pop( Deque D )
+{
+    if(D->Front!=D->Rear-1)
+    {
+        ElementType *a=0;
+        a=D->Data;
+        free(D);
+        return *a;
+    }
+    else
+    {
+        return -1;
+    }
+}
+bool Inject( ElementType X, Deque D )
+{
+    if(D->Rear==D->MaxSize-1)
+    {
+        return -1;
+    }
+    else {
+        Deque E = (Deque)malloc(sizeof(struct QNode));
+        E->Rear=D->Rear;
+        D->Rear=E;
+        E->Data[D+1]=X;
+        return 1;
+    }
+}
+ElementType Eject( Deque D )
+{
+    if(D->Front!=D->Rear-1)
+    {
+        ElementType *a;
+        a=D->Data;
+        free(D);
+        return *a;
+    }
+    else
+    {
+        return -1;
+    }
 }
